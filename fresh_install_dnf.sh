78cb840f1575c50ecaa85f7dafe64ea3030394c0
Jarred <42685268+jbettencourt10@users.noreply.github.com>
2023-11-16 14:02:48 -0500

    Added chezmoi and zsh integration

--- fresh_install_dnf.sh
+++ fresh_install_dnf.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-chezmoi init --apply jbettencourt10
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+
+# Chezmoi isntallation and zsh installation
+chezmoi init --apply jbettencourt10 &&
+
+dnf install zsh &&
+
+chsh -s $(which zsh) &&
+
+cp -r fonts/* /usr/local/share/fonts &&
+
+fc-cache -fv &&
 
 
