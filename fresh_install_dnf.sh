08807f16103d7c2b96565c9a2f43ff30e341ea2e
Jarred <42685268+jbettencourt10@users.noreply.github.com>
2023-11-16 14:22:37 -0500

    Added initial dnf check for updates

--- fresh_install_dnf.sh
+++ fresh_install_dnf.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+
 if [ "$(id -u)" -ne 0 ]; then
         echo 'This script must be run by root' >&2
         exit 1
 fi
 
+dnf check-updates
+
+dnf upgrade --refresh
+
 # Chezmoi isntallation and zsh installation
-chezmoi init --apply jbettencourt10 &&
+chezmoi init --apply jbettencourt10
 
-dnf install zsh &&
+dnf install zsh dnf-automatic
 
-chsh -s $(which zsh) &&
+chsh -s $(which zsh)
 
-cp -r fonts/* /usr/local/share/fonts &&
+cp -r fonts/* /usr/local/share/fonts
 
-fc-cache -fv &&
+fc-cache -fv
 
+systemctl enable --now dnf-automatic-install.timer
 
