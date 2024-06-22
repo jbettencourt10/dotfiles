a11e51122a7f353a4699dcf34e6d084d285e61cb
Jarred <42685268+jbettencourt10@users.noreply.github.com>
2024-06-22 17:43:57 -0400

    Update and rename fresh_install_yum.sh to fresh_install.sh

---
+++ fresh_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+linux_distro=$(lsb_release -a)
+
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+
+
+if [[ $linux_distro == *"Fedora"* ]]; then
+        yum check-update
+        yum update
+        yum install firefox fastfetch htop wireshark vim vlc gimp zsh
+        # TODO: it would be a good idea to add a few cleanup tasks here
+elif [[ $linux_distro == *"Ubuntu"* ]]; then
+        apt update
+        apt full-upgrade 
+        apt install firefox fastfetch htop wireshark vim vlc gimp zsh gnome-tweaks gnome-logs cheese notepadqq ubuntu-resitricted-extras
+        apt autoremove
+        apt clean
+
+fi
+
+# Chezmoi isntallation and zsh installation
+chezmoi init --apply jbettencourt10
+
+chsh -s $(which zsh)
+
+cp -r fonts/* /usr/local/share/fonts
+
+fc-cache -fv
+
+echo "You should now login and then out!"
+
+# TODO
+# add templates for ubuntu
+# isntall vscode, bleachbit, discord, synaptic, timeshift, nordvpn, spotify, tor
