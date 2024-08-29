361273d3b915397dd0521cb34ca126e307d869a4
Jarred Bettencourt <jbettencourt10@gmail.com>
2024-08-28 21:46:44 -0400

    autosave extenssion for nvim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -20,6 +20,15 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    {
+      "okuuva/auto-save.nvim",
+      cmd = "ASToggle", -- optional for lazy loading on command
+      event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+      opts = {
+        -- your config goes here
+        -- or just leave it empty :)
+      },
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
