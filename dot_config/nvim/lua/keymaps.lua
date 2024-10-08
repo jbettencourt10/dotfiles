aab8ce510019ac51ecc0a71dcb497ca1ab133612
Jarred Bettencourt <jbettencourt10@gmail.com>
2024-10-07 23:13:08 -0400

    Update .config/nvim/lua/keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -43,6 +43,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+local function set_filetype(pattern, filetype)
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = pattern,
+        command = "set filetype=" .. filetype,
+    })
+end
+
+set_filetype({ "docker-compose.yml" }, "yaml.docker-compose")
+
 -- Open file explorer (netrw)
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', { noremap = true, silent = true, desc = "Open file explorer" })
 
