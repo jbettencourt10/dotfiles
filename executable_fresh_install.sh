1b841b28181f5b85cb723caad75002fca5bfab86
Jarred <42685268+jbettencourt10@users.noreply.github.com>
2024-11-17 20:13:32 -0500

    Update executable_fresh_install.sh

--- executable_fresh_install.sh
+++ executable_fresh_install.sh
@@ -66,6 +66,13 @@ case "$linux_distro" in
         ;;
 esac
 
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Change default shell to zsh
 chsh -s "$(which zsh)"
 
