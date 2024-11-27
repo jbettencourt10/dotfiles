eef8dc04e9fd4251477ad7ff25f6d311bb1f3348
Jarred <42685268+jbettencourt10@users.noreply.github.com>
2024-11-27 13:21:03 -0500

    Update executable_fresh_install.sh

--- executable_fresh_install.sh
+++ executable_fresh_install.sh
@@ -46,8 +46,9 @@ case "$linux_distro" in
         sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
         sudo apt update
         sudo apt full-upgrade -y
-        sudo apt install -y firefox fastfetch htop wireshark vim vlc gimp zsh gnome-tweaks gnome-logs cheese notepadqq ubuntu-restricted-extras git unzip curl wget gpg neovim tmux git-delta synaptic ripgrep fd-find fzf kitty gnome-tweaks fonts-jetbrains-mono bleachbit wl-clipboard flatpak
-
+        sudo apt install -y firefox fastfetch htop wireshark vim vlc gimp zsh gnome-tweaks gnome-logs cheese notepadqq ubuntu-restricted-extras git unzip curl wget gpg neovim tmux git-delta synaptic ripgrep fd-find fzf gnome-tweaks bleachbit wl-clipboard flatpak
+        curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+        sudo snap install --beta nvim --classic
         # VSCode installation
         # wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
         # install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/packages.microsoft.gpg
